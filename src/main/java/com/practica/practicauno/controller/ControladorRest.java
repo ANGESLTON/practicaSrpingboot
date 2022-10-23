a544e651c8f1675dbc3950907b1e20a5ac94a0ab
Angel <jasmany_1989@hotmail.com>
2022-10-22 19:27:40 -0500

    cambio de puerto

--- src/main/java/com/practica/practicauno/controller/ControladorRest.java
+++ src/main/java/com/practica/practicauno/controller/ControladorRest.java
@@ -1,4 +1,17 @@
 package com.practica.practicauno.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ControladorRest {
+
+    @GetMapping ("/")
+    public String saludar(){
+        return "hola mundo estoy en Spring boot";
+    }
+    @GetMapping ("/about")
+    public String presentar(){
+        return "Hola, mi nombre es Ange Pineda";
+    }
 }
